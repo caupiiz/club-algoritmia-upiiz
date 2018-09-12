dd459d1a17fc7c0ef06fe48a0eb62614425a9bd7
David <davbetm@gmail.com>
2018-09-11 21:26:40 -0500

    Revisión ejercicio 1 de condiciones

--- EjerciciosEnClase/Ejercicio Condiciones 1/ejercicioCondiciones1.cpp
+++ EjerciciosEnClase/Ejercicio Condiciones 1/ejercicioCondiciones1.cpp
@@ -1,43 +1,50 @@
+/* Autor: Onder
+   Fecha: 11/09/2018
+   Nivel: Básico
+   ################################################
+*/
+
 /*
 No me acuerdo exactamente de los valores pero es algo similar
         |----
-        |  x²    x>0
+        |  x²    x > 0
         |
-f(x)=   | 3x-5   x<0
+f(x)=   | 3x - 5   x < 0
         |
-        | x+20   x=0
+        | x + 20   x = 0
         |----
-Nota: La solucion a este problema no es unica , este solo una solucion al problema
-cualquier duda, correccion o sugerencia porfavor me dicen jejje xd
+Nota: La solución a este problema no es única, esto es solo una
+propuesta de solución al problema.
 
+Cualquier duda, corrección o sugerencia por favor me dicen jejje xd
 */
 
-#include<bits/stdc++.h>//Bibliotecas
-using namespace std;//Para no estar usando a cada rato el std:: en los cout y cin
-//Funcion Principal
+#include<bits/stdc++.h> // Bibliotecas
+using namespace std; // Para no estar usando a cada rato el std:: en los cout y cin
+// Función principal
 int main(){
-    //Declaro la variables
+    // Declaro la variables
     int x;
     int resultado;
-    //Pido el valor de x
-    cin>>x;
-    //Preguntamos x es mayor que 0
-    if(x>0){
-        //Resultado es igual a x al cuadrado
-        resultado=x*x;
+    // Pido el valor de x
+    cin >> x;
+    // Preguntamos "¿x es mayor que 0?"
+    if(x > 0) {
+        // Resultado es igual a "x al cuadrado".
+        resultado = x*x;
     }
-    //Preguntamos x es menor a 0
-    else if(x<0){
-        //Resultado es igual a tres veces lo que tiene x menos 5
-        resultado=3*x-5;
+    // Preguntamos "¿x es menor a 0?"
+    else if(x < 0) {
+        // Resultado es igual a "3 veces lo que tiene x menos 5"
+        resultado = 3*x-5;
     }
-    //Si no es mayor a 0 ni menor a 0 entonces es igual a 0
+    // Si no es mayor a 0 ni menor a 0 entonces es igual a 0
     else{
         //Resultado es igual o lo que tiene x + 20
-        resultado=x+20;
-        /*En este caso tambien podriamos decir que x = 20 ya que ya sabemos que
-        x==0 por lo que nos ahorramos una operacion*/
+        resultado = x+20;
+        /* En este caso también podríamos decir que x = 20 ya que ya sabemos que
+        x == 0 por lo que nos ahorramos una operación */
     }
-    cout<<resultado<<endl;
+    cout << resultado << endl;
     return 0;
 }
