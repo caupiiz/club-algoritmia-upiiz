77739b17a0a85aebb2d2be6d589cf80d1c526773
David <davbetm@gmail.com>
2019-08-21 17:41:34 -0500

    Agregar programas semana 0 - miércoles

---
+++ EjerciciosSesiones/2020-1/semana0/operacionesBasicas.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*
+    int a = 5,b = 10;
+    double resultado;
+    //Suma
+    resultado = a + b;
+    cout<<"La suma de a y b es : "<<resultado<<endl;
+    //Resta
+    resultado = a - b;
+    cout<<"La resta de a y b es : "<<resultado<<endl;
+    //Multiplicacion
+    resultado = a * b;
+    cout<<"La Multiplicacion de a y b es : "<<resultado<<endl;
+    //Division
+    resultado = a / b;
+    cout<<"La Division de a y b es : "<<resultado<<endl;
+    //Modulo
+    resultado = a % 2;
+    cout<<"El Modulo de a y b es : "<<resultado<<endl;*/
+
+    bool a = true, b = true;
+    //AND
+
+    //OR
+
+    //XOR
+
+
+
+
+
+
+
+    return 0;
+}
