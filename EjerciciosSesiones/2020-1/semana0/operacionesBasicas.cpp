6b2bd704d463c2ad074b811278d1beae0ac6d6e4
David <davbetm@gmail.com>
2019-08-22 21:07:27 -0500

    Agregar archivo con jueces online

--- EjerciciosSesiones/2020-1/semana0/operacionesBasicas.cpp
+++ EjerciciosSesiones/2020-1/semana0/operacionesBasicas.cpp
@@ -5,34 +5,50 @@ int main(){
     /*
     int a = 5,b = 10;
     double resultado;
-    //Suma
+    // Suma
     resultado = a + b;
-    cout<<"La suma de a y b es : "<<resultado<<endl;
-    //Resta
+    cout << "La suma de a y b es : "<< resultado << endl;
+    // Resta
     resultado = a - b;
-    cout<<"La resta de a y b es : "<<resultado<<endl;
-    //Multiplicacion
+    cout << "La resta de a y b es : " << resultado << endl;
+    // Multiplicación
     resultado = a * b;
-    cout<<"La Multiplicacion de a y b es : "<<resultado<<endl;
-    //Division
+    cout << "La Multiplicacion de a y b es : " << resultado << endl;
+    // División
     resultado = a / b;
-    cout<<"La Division de a y b es : "<<resultado<<endl;
-    //Modulo
+    cout << "La Division de a y b es : " << resultado << endl;
+    // Módulo
     resultado = a % 2;
-    cout<<"El Modulo de a y b es : "<<resultado<<endl;*/
+    cout << "El Modulo de a y b es : " << resultado << endl;*/
 
     bool a = true, b = true;
-    //AND
+    bool salida;
+    //AND, &&
+    /*
+    0 and 0 = 0
+    0 and 1 = 0
+    1 and 0 = 0
+    1 and 1 = 1
+    */
+    salida = a && b;
 
-    //OR
+    // OR, ||
+    /*
+    0 or 0 = 0
+    0 or 1 = 1
+    1 or 0 = 1
+    1 or 1 = 1
+    */
+    salida = a || b;
 
-    //XOR
-
-
-
-
-
-
+    // XOR
+    /*
+    0 and 0 = 0
+    0 and 1 = 1
+    1 and 0 = 1
+    1 and 1 = 0
+    */
+    salida = a ^ b;
 
     return 0;
 }
---
+++ Info/online_judges.md
@@ -0,0 +1,11 @@
+<h4>Online judges</h4>
+
+1. [OmegaUp](https://omegaup.com/)
+2. [UVa](https://uva.onlinejudge.org/)
+3. [URI](https://www.urionlinejudge.com.br/)
+4. [HackerRank](https://www.hackerrank.com/dashboard)
+5. [GeeksForGeeks](https://www.geeksforgeeks.org/)
+6. [CodeSignal](https://codesignal.com/)
+7. [CodeForces](https://codeforces.com/)
+8. [COJ](http://coj.uci.cu/index.xhtml)
+9. [SOJ](https://www.spoj.com/)
