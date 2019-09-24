b6599ee9a5debabeab4f843fc16b70d72f64400a
David <davbetm@gmail.com>
2019-09-24 16:08:06 -0500

    Ejercicios sesión martes, semana 5

--- EjerciciosSesiones/2020-1/semana5/ascii.cpp
+++ EjerciciosSesiones/2020-1/semana5/ascii.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // Programa para imprimir los caracteres básicos y su correspondiente
 // código ASCII en décimal.
 int main() {
-
+    for (char x = '!'; x <= '~';x++) {
+        cout << (int)(x) << ":" << x << endl;
+    }
     return 0;
 }
--- EjerciciosSesiones/2020-1/semana5/cifrado.cpp
+++ EjerciciosSesiones/2020-1/semana5/cifrado.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+// Prototipo de la función
+string encriptar(string cadena);
+string desencriptar(string cadena);
 
 int main() {
-    
+    string cadena; // declaración
+    cout << "Escribe tu cadena a encriptar: ";
+    cin >> cadena;
+    string cadenaEncriptada = encriptar(cadena);
+    cout << "Tu cadena encriptada es: " << cadenaEncriptada << endl;
+    // desencriptar
+    string ejemplo = "crpelh";
+    cout << ejemplo << " -> " << desencriptar(ejemplo) << endl;
+
     return 0;
+}
+
+// Implementar la función
+string encriptar(string cadena) {
+    string resultado = "";
+    // cadena = 'hola'
+    // cadena[0] = 'h'
+    // cadena[1] = 'o'...
+    int longitud = cadena.size();
+    for (int i = 0; i < longitud; i++) {
+        int x = (int)cadena[i] - 97;
+        int e_n = (x - 3) % 26;
+        e_n += 97;
+        resultado += (char)e_n;
+    }
+    return resultado;
+}
+
+string desencriptar(string cadena) {
+    string resultado = "";
+
+    int longitud = cadena.size();
+    for (int i = 0; i < longitud; i++) {
+        int x = (int)cadena[i] - 97;
+        int e_n = (x - 3) % 26;
+        e_n = (e_n < 0) ? 24 - e_n : e_n;
+        e_n += 97;
+        resultado += (char)e_n;
+    }
+    return resultado;
 }
