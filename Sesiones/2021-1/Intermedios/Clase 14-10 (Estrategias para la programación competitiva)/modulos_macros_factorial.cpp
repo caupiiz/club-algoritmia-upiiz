5af75868e6e33e1cd56cd97e55901f6eb2eab858
Club de algoritmia UPIIZ <60445907+caupiiz@users.noreply.github.com>
2020-10-14 22:26:38 -0500

    Create modulos_macros_factorial.cpp

---
+++ Sesiones/2021-1/Intermedios/Clase 14-10 (Estrategias para la programación competitiva)/modulos_macros_factorial.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std; //biblioteca estandar de c++ para que las que la usen no ocupen
+
+
+//Macros
+#define watch(x) cout << (#x) << " es: " << x << endl;
+//this code makes input and output more efficient
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//en este, va del 1 hasta el n, no es recomendable usar para arreglos
+#define forn(i,a,b) for (int i = a; i <= b; i++) 
+//en este si empieza del 0 hasta n-1
+#define forzero(i,a,b) for (int i = a; i < b; i++) 
+
+#define SQ(a) a*a
+#define SQpro(a) (a)*(a)
+
+
+//Type names
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef vector<long long int> vlli;
+const lli MAX = 1e9+7;//primo mas grande que cabe en un int, int es 2^31 -1
+
+//usar double en vez de float por más precision
+
+int main(){
+
+    fastIO
+
+    /*
+    unsigned long long int n1=0;
+    unsigned long long int n2=0;
+    unsigned long long int n3=0;
+    */
+    ulli n1=0, n2=0, n3=0;
+
+    double a = 1;
+    double b = 0.3 * 3 + 0.1;
+
+    printf("%.20f\n", b);
+    
+
+    if(a==b){
+        //"\n" works faster than endl, because endl always causes a flush operation 
+        //cout << "Si" << endl;
+        cout << "Si \n";
+    }
+    else{
+        cout << "No \n" ;
+    }
+
+    if(are_equal(a,b)){
+        cout << "Si \n";
+    }
+    else{
+        cout << "No \n";
+    }
+    watch(a)
+    watch(b)
+
+    //Hasta aqui es modulos
+
+
+    //Desde aqui es el factorial de n, impreso modulo 10e9 + 7
+
+    lli x = 1, n = 0, m = 10e9 + 7;
+
+    cin >> n;
+
+
+    for (int i = 2; i <= n; i++) {
+        x = (x * i ) % m;
+    }
+    
+    cout << x % m << "\n";
+
+    x = 1;
+
+    forn(i,2,n) {
+        //x = x * i;
+        x *= i;
+    }
+
+    cout << x << "\n";
+    cout << x % m << "\n";
+
+    //probar con 201, 100 y luego con 8 xd
+
+
+    
+    //ojo con los macros, ejemplo:
+    cout << SQ(3+3) << "\n";
+    cout << SQpro(3+3) << "\n";
+
+
+    
+    cout << 3+3*3+3 << "\n"; // 15
+    cout << (3+3)*(3+3) << "\n"; // 36
+    */
+
+    return 0;
+}
