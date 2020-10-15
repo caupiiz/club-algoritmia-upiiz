9c047636a69706db9ba87a556702650185078427
Club de algoritmia UPIIZ <60445907+caupiiz@users.noreply.github.com>
2020-10-14 22:25:17 -0500

    Create plantilla.cpp

---
+++ Sesiones/2021-1/Intermedios/Clase 14-10 (Estrategias para la programación competitiva)/plantilla.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std; //biblioteca estandar de c++ para que las que la usen no ocupen
+
+//Macros
+#define watch(x) cout << (#x) << " es: " << x << endl;
+//this code makes input and output more efficient
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+//Type names
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef vector<long long int> vlli;
+const lli MAX = 1e9+7;//primo mas grande que cabe en un int, int es 2^31 -1
+
+//usar double en vez de float por más precision
+
+bool are_equal(double a, double b){
+  if (abs(a-b) < 1e-9) { // a and b are equal  
+    return true;
+  }
+  else{
+  	return false;
+  }
+}
+
+
+
+
+int main(){
+
+    fastIO
+    
+
+
+    return 0;
+}
